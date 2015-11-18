274f7c451dfefd8b46da4e86769ac3f1a9ebd3e8
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-11-18 14:01:52 -0800

    Center mode Carousel.

--- js/app.js
+++ js/app.js
@@ -2,32 +2,41 @@ $(document).ready(function() {
 	//Velocity(document.getElementById("dummy"), { opacity: 0.5 }, { duration: 1000 });
       $('.image-slider').slick({
         centerMode: true,
-		  centerPadding: '60px',
-		  slidesToShow: 3,
+        dots: true,
+        infinite: true,
+		centerPadding: '60px',
+		slidesToShow: 3,
+		slidesToScroll: 0,
 		  responsive: [
+		   {
+		      breakpoint: 1200,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 2,
+		        centerPadding: '40px'
+		      }
+		    },
 		    {
 		      breakpoint: 1024,
 		      settings: {
-		        slidesToShow: 3,
-		        slidesToScroll: 3,
-		        infinite: true,
-		        dots: true
+		        slidesToShow: 2,
+		        slidesToScroll: 2,
+		        centerPadding: '40px'
 		      }
 		    },
 		    {
 		      breakpoint: 768,
 		      settings: {
 		        arrows: false,
-		        centerMode: true,
 		        centerPadding: '40px',
-		        slidesToShow: 3
+		        slidesToShow: 1,
+		        slidesToScroll: 0
 		      }
 		    },
 		    {
 		      breakpoint: 480,
 		      settings: {
 		        arrows: false,
-		        centerMode: true,
 		        centerPadding: '40px',
 		        slidesToShow: 1
 		      }
@@ -36,14 +45,10 @@ $(document).ready(function() {
 		      breakpoint: 320,
 		      settings: {
 		        slidesToShow: 3,
-		        slidesToScroll: 3,
-		        infinite: true,
-		        dots: true
+		        slidesToScroll: 3
 		      }
 		    }
 		  ],
-		  slidesToShow: 3,
-		  slidesToScroll: 1,
 		  autoplay: true,
 		  autoplaySpeed: 2000
       });
