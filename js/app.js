ce84f21403f9988b61e98411879c10b28325456e
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-11-22 12:42:53 -0800

    Installed NPM and node.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 	//Velocity(document.getElementById("dummy"), { opacity: 0.5 }, { duration: 1000 });
       $('.image-slider').slick({
-        centerMode: true,
         dots: true,
-        infinite: true,
-		centerPadding: '60px',
-		slidesToShow: 3,
-		slidesToScroll: 0,
-		  responsive: [
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
 		   {
 		      breakpoint: 1200,
 		      settings: {
@@ -16,40 +17,38 @@ $(document).ready(function() {
 		        centerPadding: '40px'
 		      }
 		    },
-		    {
-		      breakpoint: 1024,
-		      settings: {
+ 		    {
+ 		      breakpoint: 1024,
+ 		      settings: {
 		        slidesToShow: 2,
 		        slidesToScroll: 2,
 		        centerPadding: '40px'
-		      }
-		    },
-		    {
-		      breakpoint: 768,
-		      settings: {
-		        arrows: false,
-		        centerPadding: '40px',
+ 		      }
+ 		    },
+ 		    {
+ 		      breakpoint: 768,
+ 		      settings: {
+ 		        arrows: false,
+ 		        centerPadding: '40px',
 		        slidesToShow: 1,
 		        slidesToScroll: 0
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        arrows: false,
-		        centerPadding: '40px',
-		        slidesToShow: 1
-		      }
-		    },
-		    {
-		      breakpoint: 320,
-		      settings: {
-		        slidesToShow: 3,
+ 		      }
+ 		    },
+ 		    {
+ 		      breakpoint: 480,
+ 		      settings: {
+ 		        arrows: false,
+ 		        centerPadding: '40px',
+ 		        slidesToShow: 1
+ 		      }
+ 		    },
+ 		    {
+ 		      breakpoint: 320,
+ 		      settings: {
+ 		        slidesToShow: 3,
 		        slidesToScroll: 3
-		      }
-		    }
-		  ],
-		  autoplay: true,
-		  autoplaySpeed: 2000
+ 		      }
+ 		    }
+ 		  ],
       });
 });
